638a2f44d205f2d82bb287d2f9d9ef6b0b87fd4b
Marc Green <m.green@surrey.ac.uk>
2021-01-13 14:46:17 +0000

    Added CRNN model

--- fed_fsd.py
+++ fed_fsd.py
@@ -1,4 +1,117 @@
-import glob
+import numpy as np, pandas as pd
+import matplotlib.pyplot as plt
+import os, glob
+import torch, torchaudio
+from torch.utils.data import Dataset, DataLoader, IterableDataset
+import seaborn as sns; sns.set()
+
+
+
+class FSD_CRNN(nn.Module):
+    '''implementation of baseline CRNN model described in FSD50k paper'''
+
+    def __init__(self):
+        super().__init__()
+
+        # three convolutional layers
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 128, kernel_size=(5,5), padding=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2,5))
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=(5,5), padding=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2,4))
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=(5,5), padding=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2,2))
+        )
+
+        # bidirectional GRU layer
+        self.bigru = nn.GRU(input_size=256, hidden_size=64, bidirectional=True)
+
+        # fully connected output layer
+        self.fc = nn.Linear(128, 200)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        # rearrange the data for recurrent layer
+        # (2 frequency bands of 128 channels stacked)
+        # GRU wants time steps, batch, features (stacked)
+        x = x.permute(2, 0, 3, 1).reshape(12, -1, 256)
+
+        # take final state as output of BiGRU
+        _, x = self.bigru(x)
+        # stack forward and backward outputs
+        x = x.permute(1, 0, 2).reshape(-1, 128)
+
+        x = self.fc(x)
+
+        return torch.sigmoid(x)
+
+
+class FSD50k_MelSpec1s(Dataset):
+    '''Dataset object to load FSD50k preprocessed into 1-second clips with
+    96-band mel-spectrogram representation'''
+
+    def __init__(self, transforms=None,
+                 anno_dir='FSD50K.ground_truth/',
+                 dev=True):
+
+        if dev: prefix = 'dev'
+        else: prefix = 'eval'
+
+        self.transforms = transforms
+
+        data_path = 'FSD50k_' + prefix + '_1sec_segs'
+
+        self.info = pd.read_csv(anno_dir + prefix + '.csv')
+
+        self.file_list = glob.glob(data_path + '/*')
+
+        vocab = pd.read_csv(anno_dir + 'vocabulary.csv', header=None)
+        self.labels = vocab[1]
+        self.mids = vocab[2]
+
+    def __len__(self): return len(self.file_list)
+
+    def __getitem__(self, item):
+
+        # load file
+        filepath = self.file_list[item]
+        x = torch.load(filepath)
+
+        # find index of original audio clip from filename
+        clip_index = int(filepath[filepath.find('/') +1:].split('.')[0])
+        csv_index = np.where(self.info['fname'] == clip_index)[0][0]
+
+        # set up array of zeros for binarised output
+        y = torch.zeros((len(vocab)), dtype=torch.bool)
+
+        # get m_ids from metadata as python list
+        tags = self.info.iloc[csv_index]['mids'].split(',')
+
+        # binarised indication of tags
+        for tag in tags:
+            y[np.where(self.mids == tag)[0][0]] = True
+
+        # probably no transforms (data already processed) but good to include
+        if self.transforms is not None:
+            x = self.transforms(x)
+
+        return x.unsqueeze(0), y
+
 
 class FSD50k(Dataset):
     '''Dataset object for FSD50K. This will load audio and labels
@@ -18,7 +131,7 @@ class FSD50k(Dataset):
 
         # read file list and annotations
         self.info = pd.read_csv(anno_dir + prefix + '.csv')
-        self.vocab = pd.read_csv(anno_dir + 'vocabulary.csv')
+        self.vocab = pd.read_csv(anno_dir + 'vocabulary.csv', header=None)
 
         self.labels = vocab[1]
         self.mids = vocab[2]
@@ -36,8 +149,7 @@ class FSD50k(Dataset):
             y[np.where(self.mids == tag)[0][0]] = True
 
         # load associated audio file
-        filepath = self.audio_path + str(
-            self.info.iloc[item]['fname']) + '.wav'
+        filepath = self.audio_path + str(self.info.iloc[item]['fname']) + '.wav'
         x = torchaudio.load(filepath)[0]
 
         if self.transforms is not None:
